ef9e4b54c4e0729ec43aa339bb02b8feaa37789f
Asad Zaki <azaki13@yahoo.com>
2022-06-12 00:39:52 -0400

    updated logic

---
+++ problem-euler5.js
@@ -0,0 +1,29 @@
+/* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? */
+
+// Step 1. Start with n = 1
+// Step 2. See if n is divisible by all numbers from 1 to maxNum
+// Step 3. If it is, return n
+
+
+function problem5(maxNum){
+    let n = 1
+    let shouldBreak = false
+    while (!shouldBreak) {
+        for(let j = 2; j <= maxNum; j++){
+            if (n % j !== 0){
+                n++
+                break;
+            }
+            if(j === maxNum) {
+                shouldBreak = true
+            }
+        }
+    }
+    return n
+}
+
+const ans = problem5(20);
+
+console.log(ans)
---
+++ problem5.js
@@ -0,0 +1,12 @@
+function smallestDivisible(maxNum, dividerMax){
+    countUp = 1
+    for (let i = 1; i <= maxNum; i++){
+        for (let j = 1; j <= dividerMax; j++){
+            if ( i / j = max)
+        }
+        console.log(i)
+    }
+
+}
+
+smallestDivisible(10)
